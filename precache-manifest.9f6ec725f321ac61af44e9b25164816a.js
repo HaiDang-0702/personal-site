ed24391f43d94a3f1fdebd164a33777bbbbfc6b0
Hai <haidangho07022000@gmail.com>
2022-08-04 16:22:27 +0700

    Updates

---
+++ precache-manifest.9f6ec725f321ac61af44e9b25164816a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ba5ca0121154ecb8c9c",
+    "url": "/personal-site/static/css/main.6d88e335.chunk.css"
+  },
+  {
+    "revision": "2ba5ca0121154ecb8c9c",
+    "url": "/personal-site/static/js/main.2ba5ca01.chunk.js"
+  },
+  {
+    "revision": "4474d6d9b405aa0a6c21",
+    "url": "/personal-site/static/css/1.afb4ec7f.chunk.css"
+  },
+  {
+    "revision": "4474d6d9b405aa0a6c21",
+    "url": "/personal-site/static/js/1.4474d6d9.chunk.js"
+  },
+  {
+    "revision": "60ebdd6b1870a2709c31",
+    "url": "/personal-site/static/js/runtime~main.60ebdd6b.js"
+  },
+  {
+    "revision": "8ffef1008e568135dae240e608d8a5f2",
+    "url": "/personal-site/static/media/Sang_Bui.8ffef100.jpg"
+  },
+  {
+    "revision": "6d562cf76d6dc2e2a56b4dbe5ba58f6b",
+    "url": "/personal-site/static/media/mobileZhangjiajie.6d562cf7.jpeg"
+  },
+  {
+    "revision": "6242ad4a60f29f54b95a84b60f83553e",
+    "url": "/personal-site/static/media/Zhangjiajie.6242ad4a.jpg"
+  },
+  {
+    "revision": "30ae1f8b21892e40a97fdb7d6abe6ecf",
+    "url": "/personal-site/index.html"
+  }
+];
